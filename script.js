5a5a88732f6e4cab24aecca819b809bd1035b6df
Rizky Anugrah <muhammadrizkyanugrah01@gmail.com>
2023-09-22 23:34:52 +0800

    Add Form On HTML And Finish Challenge javascript

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 //6. Make Function handleGetFormData For Handle From Input By User
 function handleGetFormData() {
-  var name = document.getElementById("name").value;
-  var city = document.getElementById("city").value;
-  var email = document.getElementById("email").value;
-  var zipCode = document.getElementById("zip-code").value;
-  var status = document.getElementById("status").checked;
+  let name = document.getElementById("name").value;
+  let city = document.getElementById("city").value;
+  let email = document.getElementById("email").value;
+  let zipCode = document.getElementById("zip-code").value;
+  let status = document.getElementById("status").checked;
 
   return {
     name: name,
@@ -15,9 +15,49 @@ function handleGetFormData() {
   };
 }
 
-//7.Make Function isNumber For Check Zip-Code is Not a Number
+//7. Make Function isNumber For Check Zip-Code is Not a Number
 function isNumber(string) {
   return !isNaN(string);
 }
 
-//8.
+//8. Make Function checkboxIsChecked For Check Input is Checked Or Not
+function checkboxIsChecked() {
+  let status = document.getElementById("status");
+  if (status.checked) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+//9. Make Function validateFormData
+function validateFormData(formData) {
+  if (
+    formData.name !== null &&
+    formData.city !== null &&
+    formData.email !== null &&
+    isNumber(formData.zipCode) &&
+    checkboxIsChecked(formData.status)
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+//10. Make Function Submit For Handle Proses Submit Form
+function submit() {
+  const formData = handleGetFormData();
+  if (validateFormData(formData)) {
+    document.getElementById("warning").textContent = "";
+  } else {
+    document.getElementById("warning").textContent =
+      "Periksa form anda sekali lagi";
+  }
+}
+
+// Call Function submit when form on click submit
+document.getElementById("myForm").addEventListener("submit", (event) => {
+  event.preventDefault();
+  submit();
+});
